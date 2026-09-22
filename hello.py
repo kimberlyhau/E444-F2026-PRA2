8c000434fad680e7a81fbfaecc735d079bbb2c2f
Kimberly <61749817+kimberlyhau@users.noreply.github.com>
2026-09-22 17:37:37 -0400

    1.3

--- hello.py
+++ hello.py
@@ -1,16 +1,19 @@
+from datetime import datetime, timezone
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap4
 
 app = Flask(__name__)
 
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap4(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name='Kimberly',
+                           current_time=datetime.now(timezone.utc).isoformat())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name,
+                           current_time=datetime.now(timezone.utc).isoformat())
